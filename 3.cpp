768e6772c55fe40c845e7d2e96a77734079d69e5
Ashish <ashishkumarjha9896@gmail.com>
2026-08-12 15:54:01 +0530

    lecture 2

---
+++ 3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int length,breadth;
+    cin>>length>>breadth;
+    cout<<"Area of rectangle is: "<<length*breadth<<endl;
+    
+    return 0;
+}
